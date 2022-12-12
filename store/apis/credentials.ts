e4a58410d7b9aeba6abb126c8ba5302804aea01e
Fabrice Francois <fabricevladimir@gmail.com>
2022-12-12 10:51:49 -0500

    lint

--- store/apis/credentials.ts
+++ store/apis/credentials.ts
@@ -1,29 +1,12 @@
-// import client from './client'
-
-// const getCredentials = (nodeCredentialName) => client.get('/credentials', { params: { nodeCredentialName } })
-
-// const getCredentialParams = (name) => client.get(`/node-credentials/${name}`)
-
-// const getSpecificCredential = (id, isEncrypted) => client.get(`/credentials/${id}`, { params: { isEncrypted } })
-
-// const createNewCredential = (credentialBody) => client.post(`/credentials`, credentialBody) //credentialBody: ICredential
-
-// const updateCredential = (id, credentialBody) => client.put(`/credentials/${id}`, credentialBody) //credentialBody: ICredential
-
-// const deleteCredential = (id) => client.delete(`/credentials/${id}`)
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { HYDRATE } from "next-redux-wrapper";
 
 import { backendApiBaseURL as baseUrl } from "utils/constants";
 import {
-  ICommonObject,
   ICredential,
   INodeCredential,
-  IWallet,
   IWalletResponse,
 } from "utils/interfaces";
-
-type AllWalletsResponse = Array<IWallet>;
 
 export const credentialsApi = createApi({
   reducerPath: "credentialsApi",
