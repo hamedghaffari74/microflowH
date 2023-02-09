2e1e76baa8f8f8f708b0c37f3b1b1ba8a868b715
Mahdi-Ghasemii <ghasemimahdi2002@gmail.com>
2023-02-09 19:56:50 +0330

    addd icon package and use that for icons

--- components/backIcon.tsx
+++ components/backIcon.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
-import LeftArrow from "../images/left-arrow.svg";
 import { useRouter } from "next/router";
-
+import { ArrowSquareLeft } from "iconsax-react";
 export default function BackIcon() {
    const router = useRouter();
 
    const backHistoryHanlder = () => {
       router.back();
    };
-   return (
-      <button onClick={backHistoryHanlder} style={{ backgroundColor: "inherit", border: "none", cursor: "pointer" }}>
-         <Image src={LeftArrow} alt="left subquare arrow"></Image>
-      </button>
-   );
+   return <ArrowSquareLeft cursor="pointer" onClick={backHistoryHanlder} size="42" />;
 }
